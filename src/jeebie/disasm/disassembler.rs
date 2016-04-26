fec9705ddaf9d5e4b21dff6b20c2ecfc1ee4240d
Valerio Pipolo <valep27@gmail.com>
2016-04-26 12:03:37 +0200

    Changed Disassembler to have immutable interface

--- src/jeebie/disasm/disassembler.rs
+++ src/jeebie/disasm/disassembler.rs
@@ -1,19 +1,22 @@
-
+//! A simple disassembler, useful for showing the actual assembly code and eventually debugging.
 
 use jeebie::disasm::metadata::{ CB_DISASM_TABLE, DISASM_TABLE };
+use std::cell::Cell;
 
+/// The disassembler struct, it has internal mutable state required to disassemble a binary file
+/// from start to finish, but exposes an immutable interface.
 pub struct Disassembler {
-    pc: usize,
+    pc: Cell<usize>,
 }
 
 impl Disassembler {
 
     pub fn new() -> Self {
-        Disassembler { pc: 0 }
+        Disassembler { pc: Cell::new(0) }
     }
 
     pub fn rom_to_string(data: &[u8]) -> Result<String, String> {
-        let mut d = Disassembler::new();
+        let d = Disassembler::new();
         let mut out = String::new();
 
         while let Ok(instr) = d.get_instruction_str(data) {
@@ -23,12 +26,12 @@ impl Disassembler {
         Ok(out)
     }
 
-    fn get_opcode_name(&mut self, data: &[u8]) -> Result<String, String> {
-        match data.get(self.pc) {
+    fn get_opcode_name(&self, data: &[u8]) -> Result<String, String> {
+        match data.get(self.pc.get()) {
             Some(&0xCB) => {
-                self.pc = self.pc.wrapping_add(1);
+                self.pc.set(self.pc.get().wrapping_add(1));
 
-                if let Some(second_byte) = data.get(self.pc) {
+                if let Some(second_byte) = data.get(self.pc.get()) {
                     Ok(String::from(CB_DISASM_TABLE[*second_byte as usize]))
                 } else {
                     Err(String::from("2 Byte opcode incomplete (CB prefix)"))
@@ -41,16 +44,16 @@ impl Disassembler {
 
     /// Returns an instruction string with the appropriate immediate
     /// values filled in.
-    pub fn get_instruction_str(&mut self, data: &[u8]) -> Result<String, String> {
-        let pc = self.pc;
+    pub fn get_instruction_str(&self, data: &[u8]) -> Result<String, String> {
+        let pc = self.pc.get();
         let mut instr = match self.get_opcode_name(data) {
             Ok(s) => s,
             Err(s) => return Err(s),
         };
 
-        let low = data.get(self.pc.wrapping_add(1)).map(|x| *x as u16);
-        let high = data.get(self.pc.wrapping_add(2)).map(|x| (*x as u16) << 8);
-        self.pc = self.pc.wrapping_add(1);
+        let low = data.get(self.pc.get().wrapping_add(1)).map(|x| *x as u16);
+        let high = data.get(self.pc.get().wrapping_add(2)).map(|x| (*x as u16) << 8);
+        self.pc.set(self.pc.get().wrapping_add(1));
 
         if instr.contains("nn") {
             let immediate16 = match (low, high) {
@@ -60,7 +63,7 @@ impl Disassembler {
 
             let nn = format!("${:04x}", immediate16);
             instr = instr.replace("nn", &nn);
-            self.pc = self.pc.wrapping_add(2);
+            self.pc.set(self.pc.get().wrapping_add(2));
         }
 
         if instr.contains("n") || instr.contains("*") {
@@ -74,7 +77,7 @@ impl Disassembler {
 
             instr = instr.replace("n", &n);
             instr = instr.replace("*", &n_signed);
-            self.pc = self.pc.wrapping_add(1);
+            self.pc.set(self.pc.get().wrapping_add(1));
         }
 
         Ok(format!("{:<20}; ${:04x}", instr, pc))
@@ -83,46 +86,46 @@ impl Disassembler {
 
 #[test]
 fn test_single_instr() {
-    let mut d = Disassembler::new();
+    let d = Disassembler::new();
     let data: [u8; 2] = [0x00, 0x07];
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("NOP"));
-    assert_eq!(1, d.pc);
+    assert_eq!(1, d.pc.get());
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("RLCA"));
-    assert_eq!(2, d.pc);
+    assert_eq!(2, d.pc.get());
 }
 
 #[test]
 fn test_immediate8_instr() {
-    let mut d = Disassembler::new();
+    let d = Disassembler::new();
     let data: [u8; 4] = [0x26, 0x07, 0x20, 0xFF];
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("LD H,$07"));
-    assert_eq!(2, d.pc);
+    assert_eq!(2, d.pc.get());
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("JR NZ,-1"));
-    assert_eq!(4, d.pc);
+    assert_eq!(4, d.pc.get());
 }
 
 #[test]
 fn test_immediate16_instr() {
-    let mut d = Disassembler::new();
+    let d = Disassembler::new();
     let data: [u8; 5] = [0x31, 0x20, 0x04, 0x00, 0x04];
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("LD SP,$0420"));
-    assert_eq!(3, d.pc);
+    assert_eq!(3, d.pc.get());
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("NOP"));
-    assert_eq!(4, d.pc);
+    assert_eq!(4, d.pc.get());
 
     let instr = d.get_instruction_str(&data).unwrap();
     assert!(instr.starts_with("INC B"));
-    assert_eq!(5, d.pc);
+    assert_eq!(5, d.pc.get());
 }
