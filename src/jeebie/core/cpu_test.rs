ed76e9361020b42f065fa22bbe2e270188869315
Valerio Pipolo <valep27@gmail.com>
2016-05-03 17:28:42 +0200

    Added new tests for cpu

--- src/jeebie/core/cpu_test.rs
+++ src/jeebie/core/cpu_test.rs
@@ -1,10 +1,11 @@
 use jeebie::core::cpu::CPU;
 use jeebie::memory::MMU;
-use jeebie::core::registers::*;
 use jeebie::core::registers::Register8::*;
+use jeebie::core::registers::Register16::*;
+use jeebie::core::registers::Flags::*;
 
 #[test]
-fn compute_add_8_bit() {
+fn add_8_bit() {
     let mut mmu = MMU::new();
     let mut cpu = CPU::new(&mut mmu);
     // simple add, 2 + 2
@@ -12,10 +13,10 @@ fn compute_add_8_bit() {
     cpu.compute_add(A, A);
     assert_eq!(cpu.get8(A), 4u8);
     // no flags
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(!cpu.reg.is_set(Flags::HalfCarry));
-    assert!(!cpu.reg.is_set(Flags::Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(!cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
 
     // overflow, 255 + 1
     cpu.set8(A, 255u8);
@@ -23,10 +24,10 @@ fn compute_add_8_bit() {
     cpu.compute_add(A, B);
     assert_eq!(cpu.get8(A), 0u8);
     // all except sub
-    assert!(cpu.reg.is_set(Flags::Zero));
-    assert!(cpu.reg.is_set(Flags::Carry));
-    assert!(cpu.reg.is_set(Flags::HalfCarry));
-    assert!(!cpu.reg.is_set(Flags::Sub));
+    assert!(cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(Carry));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
 
     // halfcarry
     cpu.set8(A, 127u8);
@@ -34,14 +35,14 @@ fn compute_add_8_bit() {
     cpu.compute_add(A, B);
     assert_eq!(cpu.get8(A), 128u8);
     // HC only
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(cpu.reg.is_set(Flags::HalfCarry));
-    assert!(!cpu.reg.is_set(Flags::Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
 }
 
 #[test]
-fn compute_sub_8_bit() {
+fn sub_8_bit() {
     let mut mmu = MMU::new();
     let mut cpu = CPU::new(&mut mmu);
     // simple sub
@@ -50,10 +51,10 @@ fn compute_sub_8_bit() {
     cpu.compute_sub(B);
     assert_eq!(cpu.get8(A), 6u8);
     // sub only
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(!cpu.reg.is_set(Flags::HalfCarry));
-    assert!(cpu.reg.is_set(Flags::Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(!cpu.reg.is_set(HalfCarry));
+    assert!(cpu.reg.is_set(Sub));
 
     // underflow
     cpu.set8(A, 0u8);
@@ -61,10 +62,10 @@ fn compute_sub_8_bit() {
     cpu.compute_sub(B);
     assert_eq!(cpu.get8(A), 255u8);
     // all flags except zero
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(cpu.reg.is_set(Flags::Carry));
-    assert!(cpu.reg.is_set(Flags::HalfCarry));
-    assert!(cpu.reg.is_set(Flags::Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(Carry));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(cpu.reg.is_set(Sub));
 
     // halfcarry
     cpu.set8(A, 128u8);
@@ -72,14 +73,14 @@ fn compute_sub_8_bit() {
     cpu.compute_sub(B);
     assert_eq!(cpu.get8(A), 126u8);
     // HC and sub
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(cpu.reg.is_set(Flags::HalfCarry));
-    assert!(cpu.reg.is_set(Flags::Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(cpu.reg.is_set(Sub));
 }
 
 #[test]
-fn compute_cp_test() {
+fn cp_test() {
     let mut mmu = MMU::new();
     let mut cpu = CPU::new(&mut mmu);
 
@@ -88,18 +89,98 @@ fn compute_cp_test() {
     cpu.set8(B, 2);
     cpu.compute_cp(B);
 
-    assert!(cpu.reg.is_set(Flags::Sub));
-    assert!(!cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(!cpu.reg.is_set(Flags::HalfCarry));
+    assert!(cpu.reg.is_set(Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(!cpu.reg.is_set(HalfCarry));
 
     // underflow
     cpu.set8(A, 100);
     cpu.set8(B, 100);
     cpu.compute_cp(B);
 
-    assert!(cpu.reg.is_set(Flags::Zero));
-    assert!(!cpu.reg.is_set(Flags::Carry));
-    assert!(!cpu.reg.is_set(Flags::HalfCarry));
-    assert!(cpu.reg.is_set(Flags::Sub));
+    assert!(cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Carry));
+    assert!(!cpu.reg.is_set(HalfCarry));
+    assert!(cpu.reg.is_set(Sub));
+}
+
+#[test]
+fn inc_test() {
+    let mut mmu = MMU::new();
+    let mut cpu = CPU::new(&mut mmu);
+
+    // NOTE: Carry flag is unmodified
+    // simple inc
+    cpu.set8(A, 0x9);
+    cpu.compute_inc(A);
+
+    assert_eq!(0xA, cpu.get8(A));
+    assert!(!cpu.reg.is_set(Sub));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(HalfCarry));
+
+    // zero
+    cpu.set8(A, 0xFF);
+    cpu.compute_inc(A);
+
+    assert_eq!(0, cpu.get8(A));
+    assert!(cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
+
+    // halfcarry
+    cpu.set8(A, 0x0F);
+    cpu.compute_inc(A);
+
+    assert_eq!(0x10, cpu.get8(A));
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
+}
+
+#[test]
+fn bit_check_test() {
+    let mut mmu = MMU::new();
+    let mut cpu = CPU::new(&mut mmu);
+
+    cpu.set8(B, 0b1000_0001u8);
+    cpu.bit_check(0, B);
+    // Sub is always 0, HC always 1
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
+
+    cpu.bit_check(1, B);
+    // Sub is always 0, HC always 1
+    assert!(cpu.reg.is_set(Zero));
+    assert!(cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Sub));
+}
+
+#[test]
+fn swap_test() {
+    let mut mmu = MMU::new();
+    let mut cpu = CPU::new(&mut mmu);
+
+    cpu.set8(B, 0xAB);
+    cpu.compute_swap(B);
+
+    assert_eq!(0xBA, cpu.get8(B));
+    // all reset except zero
+    assert!(!cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Sub));
+    assert!(!cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Carry));
+
+    // internal RAM, will be initialized to 0
+    cpu.set16(HL, 0xC000);
+    cpu.compute_swap(RegisterAddress(HL));
+
+    assert_eq!(0, cpu.get8(RegisterAddress(HL)));
+    // Zero should be set
+    assert!(cpu.reg.is_set(Zero));
+    assert!(!cpu.reg.is_set(Sub));
+    assert!(!cpu.reg.is_set(HalfCarry));
+    assert!(!cpu.reg.is_set(Carry));
 }
