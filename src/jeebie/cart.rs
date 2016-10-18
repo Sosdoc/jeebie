e1f9dfd4ae60bce02c2403588e0ea30e70d59afe
Valerio Pipolo <valep27@gmail.com>
2016-10-18 18:33:29 +0200

    Added some cartridge metadata processing

--- src/jeebie/cart.rs
+++ src/jeebie/cart.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
 use std::io::Read;
 
+use jeebie::utils::combine_as_u16;
+
 /// A struct representing data contained in a gameboy cartridge (a.k.a. ROM).
 /// Each Cartridge has an header with metadata (name, available hw on cart like rumble, ram, etc.)
 /// Data contained in a cartridge is exposed as a Vec<u8> but should be accessed via the MMU.
 #[derive(Debug)]
 pub struct Cartridge {
     pub size: usize,
+    pub name: String,
     pub data: Vec<u8>,
 }
 
@@ -18,8 +21,40 @@ impl Cartridge {
 
     /// Creates a new Cartridge struct from a vector buffer.
     pub fn new_with_vec(data: Vec<u8>) -> Cartridge {
+        let name_data : Vec<u8> = (&data[0x134..0x143]).to_vec();
+
+        // read name data as ASCII string
+        let name : String = name_data.into_iter()
+            .map(| c | { c as char })
+            .collect();
+
+        let cgb_flag = data[0x143];
+
+        let licensee_code_data : Vec<u8> = (&data[0x144..0x145]).to_vec();
+
+        // read licensee code as ASCII string
+        let name : String = licensee_code_data.into_iter()
+            .map(| c | { c as char })
+            .collect();
+
+        let sgb_flag = data[0x146];
+
+        let mbc_type = data[0x147];
+        let rom_size = data[0x148];
+        let ram_size = data[0x149];
+
+        let dest_code = data[0x14A];
+        let lic_code = data[0x14B];
+        let version = data[0x14C];
+
+        // only header_checksum is verified in real hardware
+        let header_checksum = data[0x14D];
+        let checksum = combine_as_u16(data[0x14E], data[0x14F]);
+
+        // TODO: process the read data and save it in the cart struct
         Cartridge {
             size: data.len(),
+            name: name,
             data: data,
         }
     }
